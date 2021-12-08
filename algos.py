d9da5ebf34233d7bee17d883a00d80777c717954
Mark de Courcy Ling <54800588+markdecl@users.noreply.github.com>
2021-12-08 15:39:08 +0000

    Initial commit

---
+++ algos.py
@@ -0,0 +1,13 @@
+def bubble_sort(our_list):
+    # We want to stop passing through the list
+    # as soon as we pass through without swapping any elements
+    has_swapped = True
+
+    while(has_swapped):
+        has_swapped = False
+        for i in range(len(our_list) - 1):
+            if our_list[i] > our_list[i+1]:
+                # Swap
+                our_list[i], our_list[i+1] = our_list[i+1], our_list[i]
+                has_swapped = True
+    return our_list

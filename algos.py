a858a76e99164ba9ac9486e1be7a508f615d4958
Mark de Courcy Ling <54800588+markdecl@users.noreply.github.com>
2021-12-09 22:05:48 +0000

    Update algos.py

--- algos.py
+++ algos.py
@@ -17,7 +17,7 @@ def bubble_sort(our_list):
 def insertionSort(array):
     # iterate through the items in the unsorted array, i.e. all but the first item
     for i in range(1, len(array)):
-        # iterate down through the sorted array + the first item in the sorted array
+        # iterate down through the sorted array + the first item in the unsorted array
         j = i
         # if the current item in this array is smaller than the next item, swap them
         while j > 0 and array[j] < array[j - 1]:
